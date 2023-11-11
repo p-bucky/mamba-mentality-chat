92d95212f55f4f8a48fef1045b91202d1d1a860c
Prashant Jha <prashanttechjha@gmail.com>
2023-11-11 23:36:23 +0530

    [whatever] not this project related but important

---
+++ git-login.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <home|office>"
+    exit 1
+fi
+
+location=$1
+
+if [ "$location" == "home" ]; then
+    ssh-add ~/.ssh/id_rsa_p_bucky
+    git config --global user.email "prashanttechjha@gmail.com"
+    git config --global user.username "Bucky"
+
+    git config --global user.email
+    git config --global user.username
+    echo "Logged in for home."
+elif [ "$location" == "office" ]; then
+    ssh-add ~/.ssh/id_rsa
+    git config --global user.email "prashant.jha@junio.in"
+    git config --global user.username "prashant.junio"
+    
+    git config --global user.email
+    git config --global user.username
+    echo "Logged in for office."
+else
+    echo "Invalid location. Please use 'home' or 'office'."
+    exit 1
+fi
